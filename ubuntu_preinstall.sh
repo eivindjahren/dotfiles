735c2b728b7ce17c78934df1546a251c1e227d7c
Eivind Jahren <eivind.jahren@gmail.com>
2020-11-29 18:54:00 +0100

    Add preinstall script for ubuntu

---
+++ ubuntu_preinstall.sh
@@ -0,0 +1,10 @@
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install neovim curl python-dev python-pip python3-dev python3-pip
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
